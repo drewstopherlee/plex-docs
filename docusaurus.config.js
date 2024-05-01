7f2b936cf6e91bc2e6db83fe531905567495297c
Andrew Shaffer <shafferison@gmail.com>
2024-05-01 00:45:43 +0000

    remove broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
@@ -122,10 +122,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
